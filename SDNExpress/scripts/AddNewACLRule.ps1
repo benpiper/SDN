0243bdb55baa862869c05dafeb7464e73bd9648b
Ben Piper <ben@benpiper.com>
2016-11-13 15:29:57 -0500

    Sorted ACL rules output

--- SDNExpress/scripts/AddNewACLRule.ps1
+++ SDNExpress/scripts/AddNewACLRule.ps1
@@ -1,5 +1,8 @@
 ﻿### Add ACL rule to deny RDP access inbound into the webtier subnet
 
+e:
+cd E:\sdn\SDNExpress\scripts
+
 . .\NetworkControllerRESTWrappers.ps1 -ComputerName nc1.company.pri
 
 #Get virtual network
@@ -7,18 +10,18 @@ $vnet = Get-NCVirtualNetwork
 $vnet
 
 #Get virtual subnet
-$vsn = Get-NCVirtualSubnet -VirtualNetwork $vnet
-$vsn
+$webtiersubnet = Get-NCVirtualSubnet -VirtualNetwork $vnet | where {$_.resourceId -eq "WebTier_Subnet"}
+$webtiersubnet
 
 #Get ACL resource reference of webtier subnet
-$aclref = $vsn[0].properties.accessControlList.resourceRef
+$aclref = $webtiersubnet.properties.accessControlList.resourceRef
 $aclref
 
 #Get ACL
 $acl = Get-NCAccessControlList | where {$_.resourceRef -eq $aclref}
 
 #View rules
-$acl.properties.aclRules.properties | ft
+$acl.properties.aclRules.properties | Sort-Object -Property priority | ft
 
 #Create a new rule to deny RDP into webtier subnet
 $acl.properties.aclRules += New-NCAccessControlListRule -Protocol "tcp" -SourcePortRange "0-65535" -DestinationPortRange "3389" -SourceAddressPrefix "*" -DestinationAddressPrefix "*" -Action "Deny" -Logging $true -ACLType "inbound" -Priority 101
@@ -28,6 +31,6 @@ New-NCAccessControlList -ResourceID $acl.resourceId -AccessControlListRules $acl
 
 #Verify the ACL has the new rule
 $acl = Get-NCAccessControlList | where {$_.resourceRef -eq $aclref}
-$acl.properties.aclRules.properties | ft
+$acl.properties.aclRules.properties | Sort-Object -Property priority | ft
 
-#Test RDP connectivity
+#Test RDP connectivity from Db server
