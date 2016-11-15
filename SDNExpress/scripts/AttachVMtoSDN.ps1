b74a0ea9f0f47edd4cf003485e24f2346a92e51b
Ben Piper <ben@benpiper.com>
2016-11-15 12:26:01 -0500

    Working script to attach VM to vsubnet

--- SDNExpress/scripts/AttachVMtoSDN.ps1
+++ SDNExpress/scripts/AttachVMtoSDN.ps1
@@ -3,14 +3,17 @@
 #Prerequisites: VM already built using lability
 
 #Shut down VM and move to sdnswitch
+$na = Get-VMNetworkAdapter -VMName DC1
+Connect-VMNetworkAdapter -SwitchName "sdnSwitch" -VMNetworkAdapter $na
 
 #Grab NC functions
-. "$($node.InstallSrcDir)\scripts\NetworkControllerRESTWrappers.ps1" -ComputerName $node.NetworkControllerRestName -UserName $node.NCClusterUserName -Password $node.NCClusterPassword
+. .\NetworkControllerRESTWrappers.ps1 -ComputerName nc1.company.pri -UserName "company\administrator" -Password "Tr@ining123"
 
-$vnet = Get-NCVirtualNetwork -ResourceId "$($node.TenantName)_$($network.ID)" #probably company_vnet1
-$vsubnet = Get-NCVirtualSubnet -VirtualNetwork $vnet -ResourceId $Network.Subnets[$using:VMInfo.subnet].ID #dbtier_subnet
+$vnet = Get-NCVirtualNetwork -ResourceId "company_vnet1" #"$($node.TenantName)_$($network.ID)" #probably company_vnet1
+$vsubnet = Get-NCVirtualSubnet -VirtualNetwork $vnet -ResourceId "dbtier_subnet" #$Network.Subnets[$using:VMInfo.subnet].ID #dbtier_subnet
 #can resourceid (guid?) below be randomly generated on the fly and used?
 #MAC address must be static and match the VMs actual MAC
-$vnic = New-NCNetworkInterface -resourceId $using:VMInfo.ResourceId -Subnet $vsubnet -IPAddress $using:VMInfo.IPAddress -MACAddress $using:VMInfo.MACAddress -DNSServers $network.DNSServers
-$vnicInstanceId = Get-NCNetworkInterfaceInstanceId -ResourceId $using:VMInfo.ResourceId
-Set-PortProfileId -resourceID ($vnicInstanceId) -VMName ($using:vmInfo.VMName) -ComputerName $using:hostNode.NodeName
+$newguid = [System.Guid]::NewGuid().toString()
+$vnic = New-NCNetworkInterface -resourceId "$newguid" -Subnet $vsubnet -IPAddress "192.168.80.9" -MACAddress "001DC8B70110" #-DNSServers $network.DNSServers
+$vnicInstanceId = Get-NCNetworkInterfaceInstanceId -ResourceId $newguid
+Set-PortProfileId -resourceID ($vnicInstanceId) -VMName "DC1" #-ComputerName $using:hostNode.NodeName
